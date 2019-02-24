de8f6b301377fd0a66c1e0ea7a9bf847a6ceae93
Nic McPhee <mcphee@morris.umn.edu>
2019-02-23 23:00:46 -0600

    Improve organization of the `Server` code
    This is a fairly substantial refactoring of the top-level server code.
    Instead of having everything just shoved in `main()`, this breaks
    things up into hopefully more coherent pieces, and in a way that
    suggests how folks might want to continue to organize things as they
    move forward.

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -14,26 +14,24 @@ import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
-  private static final String userDatabaseName = "dev";
-  private static final int serverPort = 4567;
+  private static final String DATABASE_NAME = "dev";
+  private static final int SERVER_PORT = 4567;
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
+    configureSpark();
+    defineRedirects();
+    defineDemoRoutes();
+    defineRoutes();
+  }
 
-    MongoClient mongoClient = new MongoClient();
-    MongoDatabase userDatabase = mongoClient.getDatabase(userDatabaseName);
-
-    UserController userController = new UserController(userDatabase);
-    UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
-
-    //Configure Spark
-    port(serverPort);
+  private static void configureSpark() {
+    port(SERVER_PORT);
     enableDebugScreen();
 
     // Specify where assets like images will be "stored"
     staticFiles.location("/public");
 
     options("/*", (request, response) -> {
-
       String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
       if (accessControlRequestHeaders != null) {
         response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
@@ -47,32 +45,10 @@ public class Server {
       return "OK";
     });
 
-    before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-
-
-    // Simple example route
-    get("/hello", (req, res) -> "Hello World");
-
-    // Redirects for the "home" page
-    redirect.get("", "/");
-
-    redirect.get("/", "http://localhost:9000");
-
-    /// User Endpoints ///////////////////////////
-    /////////////////////////////////////////////
-
-    //List users, filtered using query parameters
-
-    get("api/users", userRequestHandler::getUsers);
-    get("api/users/:id", userRequestHandler::getUserJSON);
-    get("api/userSummary", userRequestHandler::getUserSummary);
-    post("api/users/new", userRequestHandler::addNewUser);
-
-    // An example of throwing an unhandled exception so you can see how the
-    // Java Spark debugger displays errors like this.
-    get("api/error", (req, res) -> {
-      throw new RuntimeException("A demonstration error");
-    });
+    // Call before each request. This is necessary for the Angular code to be
+    // able to make requests of the server.
+    before((request, response)
+      -> response.header("Access-Control-Allow-Origin", "*"));
 
     // Called after each request to insert the GZIP header into the response.
     // This causes the response to be compressed _if_ the client specified
@@ -80,6 +56,41 @@ public class Server {
     // There's a similar "before" method that can be used to modify requests
     // before they they're processed by things like `get`.
     after("*", Server::addGzipHeader);
+  }
+
+  // Enable GZIP for all responses
+  private static void addGzipHeader(Request request, Response response) {
+    response.header("Content-Encoding", "gzip");
+  }
+
+  private static void defineRedirects() {
+    redirect.get("", "/");
+    // This redirects attempts to talk to the server without and `api/...` URL
+    // back to Angular.
+    redirect.get("/", "http://localhost:9000");
+  }
+
+  /*
+   * These are just here as examples. Feel free to delete this method
+   * and its routes as you build our your code.
+   */
+  private static void defineDemoRoutes() {
+    // Simple example route
+    get("/hello", (req, res) -> "Hello World");
+
+    // An example of throwing an unhandled exception so you can see how the
+    // Java Spark debugger displays errors like this. You want to remove
+    // this from your code.
+    get("api/error", (req, res) -> {
+      throw new RuntimeException("A demonstration error");
+    });
+  }
+
+  private static void defineRoutes() {
+    MongoClient mongoClient = new MongoClient();
+    MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+
+    defineUserRoutes(database);
 
     // Handle "404" file not found requests:
     notFound((req, res) -> {
@@ -89,8 +100,13 @@ public class Server {
     });
   }
 
-  // Enable GZIP for all responses
-  private static void addGzipHeader(Request request, Response response) {
-    response.header("Content-Encoding", "gzip");
+  private static void defineUserRoutes(MongoDatabase database) {
+    UserController userController = new UserController(database);
+    UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
+
+    get("api/users", userRequestHandler::getUsers);
+    get("api/users/:id", userRequestHandler::getUserJSON);
+    get("api/userSummary", userRequestHandler::getUserSummary);
+    post("api/users/new", userRequestHandler::addNewUser);
   }
 }
